9b98ac7af8014c0539892067e721b04f6ab6f064
weimingjcode <waczjm@126.com>
2020-04-26 17:17:56 +0800

    8002。。

--- cloud-provider-payment8002/src/main/java/com/rong/spcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/rong/spcloud/controller/PaymentController.java
@@ -9,6 +9,7 @@ import com.rong.spcloud.entity.Payment;
 import com.rong.spcloud.entity.deal.CommonResult;
 import com.rong.spcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -20,19 +21,22 @@ public class PaymentController {
     @Resource
     PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String port;
+
     @PostMapping(value = "/payment/creatPayment")
     public CommonResult creatPayment(@RequestBody Payment payment){
         int rst = paymentService.creatPayment(payment);
         log.info("payment插入结果---"+rst);
-        if (rst > 0) return new CommonResult<>(200,"插入成功",payment);
-        else return new CommonResult<>(444,"插入失败",null);
+        if (rst > 0) return new CommonResult<>(200,"插入成功--port--"+port,payment);
+        else return new CommonResult<>(444,"插入失败--"+port,null);
     }
 
     @GetMapping(value = "/payment/getPaymentById/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id){
         Payment payment = paymentService.getPaymentById(id);
         log.info("查询结果--"+payment);
-        if (payment != null) return new CommonResult(200,"查询成功",payment);
-        else return new CommonResult(444,"查询失败 id=" + id,null);
+        if (payment != null) return new CommonResult(200,"查询成功--port--"+port,payment);
+        else return new CommonResult(444,"查询失败 id=" + id+"port--"+port,null);
     }
 }
